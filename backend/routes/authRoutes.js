985192e2c79a4f2134206cbcd414d862d7929b5e
12aura <auraannu@gmail.com>
2025-12-29 04:11:17 +0530

    Day 2 complete - Auth + DB + JWT setup

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,3 +1,4 @@
+const authMiddleware = require("../middleware/authMiddleware");
 const express = require("express");
 const { registerUser, loginUser } = require("../controllers/authController");
 const router = express.Router();
@@ -6,3 +7,7 @@ router.post("/register", registerUser);
 router.post("/login", loginUser);
 
 module.exports = router;
+
+router.get("/me", authMiddleware, async (req, res) => {
+  res.json({ msg: "Protected Route Working", userId: req.user });
+});
