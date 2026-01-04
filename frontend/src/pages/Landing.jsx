7e27ecdccf26e9dbe504275401f5633c868de5c4
12aura <auraannu@gmail.com>
2026-01-05 00:10:28 +0530

    app added

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,1 +1,8 @@
+export default function Landing(){
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Welcome to SkillSwap</h1>
+    </div>
+  );
+}
 
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,4 @@
+export default function Login(){
+  return <h1 className="text-3xl font-bold p-5">Login Page</h1>;
+}
+
