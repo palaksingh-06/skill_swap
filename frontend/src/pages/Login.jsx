d91f1249fdbc29e1e0932b8d6928450d98c42ad7
12aura <auraannu@gmail.com>
2026-01-04 23:53:49 +0530

    Nothing void

--- frontend/src/pages/Login.jsx
+++
@@ -1,7 +0,0 @@
-export default function Landing() {
-  return (
-    <div className="h-screen flex justify-center items-center">
-      <h1 className="text-4xl font-bold">Welcome to SkillSwap</h1>
-    </div>
-  );
-}
