b641d9a05117497dd7f0fbe182c894aaac2abe79
12aura <auraannu@gmail.com>
2026-01-04 23:23:30 +0530

    Deleted the content of app.jsx and main.jsx

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,7 @@
+export default function Landing() {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <h1 className="text-4xl font-bold">Welcome to SkillSwap</h1>
+    </div>
+  );
+}
