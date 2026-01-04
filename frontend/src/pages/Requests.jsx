6d2239cf88ce5d7a0ffe139d7e9c2924d97d20cc
Ayushi Singh <ayushisingh262005@gmail.com>
2026-01-05 00:41:15 +0530

    Addede something

--- frontend/src/pages/Requests.jsx
+++ frontend/src/pages/Requests.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Profile = () => {
+const Requests = () => {
   return (
     <div>
       Requests Page
@@ -8,4 +8,4 @@ const Profile = () => {
   );
 };
 
-export default Profile;
+export default Requests;
