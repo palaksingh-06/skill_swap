690a320c88959661a44ef67f052d7e00a27dc6a2
12aura <auraannu@gmail.com>
2026-01-04 22:26:24 +0530

    frontend setup done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Routes } from 'react-router'
+
+const App = () => {
+  return (
+    <div className="h-screen" data-theme="coffee"><button className="btn" >Button</button>
+      <Routes>
+        
+      </Routes>
+    </div>
+  )
+}
+
+export default App
